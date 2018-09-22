c56e90ac1f0164ad930d43ea7af1bb0af7df838b
ALex <theadl1994@gmail.com>
2018-09-21 21:25:25 -0500

    add inputs

--- twilio_goodmorning.py
+++ twilio_goodmorning.py
@@ -15,18 +15,24 @@ account_sid = os.getenv('ACCOUNT_SID')
 auth_token = os.getenv('AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
-message = client.messages \
-                .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_=os.getenv('TWILIO_PHONE'),
-                     to='+18178995567'
-                 )
+#message = client.messages \
+#                .create(
+#                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+#                     from_=os.getenv('TWILIO_PHONE'),
+#                     to='+18178995567'
+#                 )
 
-print(message.sid)
+#print(message.sid)
 
 
-# prompt the user the phone number
+phoneNum = input('What phone number would you like to prank?: ')
+name = input('What is the name of the person\'s number you gave us?: ')
+entry = ('The number {} has been created for {}.'.format(phoneNum,name))
+print(entry)
+
+#print('You have successfully created a phone number for ' + name)
 # validated the phone number
+
 # propmt user to send message
 # add the variables to request object
 
